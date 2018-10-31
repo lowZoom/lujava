e9fd10885684fc06dbfbe1ad30050ec84301e98c
luj <david_lu_st@163.com>
2018-10-31 11:46:38 +0800

    增加lujconfig启动入口代码

--- lujava/src/main/java/luj/ava/spring/InternalContextImpl.java
+++ lujava/src/main/java/luj/ava/spring/InternalContextImpl.java
@@ -1,10 +1,7 @@
 package luj.ava.spring;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
-
-@ComponentScan(includeFilters = @ComponentScan.Filter(Internal.class))
 final class InternalContextImpl implements InternalContext {
 
   InternalContextImpl(Class<?> config) {
@@ -14,7 +11,7 @@ final class InternalContextImpl implements InternalContext {
   @Override
   public <T> T getRootBean(Class<T> beanType) {
     try (AnnotationConfigApplicationContext appCtx =
-        new AnnotationConfigApplicationContext(InternalContextImpl.class, _config)) {
+        new AnnotationConfigApplicationContext(_config)) {
       return appCtx.getBean(beanType);
     }
   }
