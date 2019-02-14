767bfe3ea9fcdf673e8c9b538a30847cdd2e5764
luj <david_lu_st@163.com>
2019-02-14 16:55:33 +0800

    完善协议字段设置逻辑

--- lujava/src/main/java/luj/ava/reflect/generic/GenericType.java
+++ lujava/src/main/java/luj/ava/reflect/generic/GenericType.java
@@ -1,15 +1,21 @@
 package luj.ava.reflect.generic;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 
 public interface GenericType {
 
+  static GenericType of(Type type) {
+    return new GenericTypeImpl((ParameterizedType) type);
+  }
+
   static GenericType ofField(Field field) {
-    return GenericTypeOfField.SINGLETON.create(field);
+    return of(field.getGenericType());
   }
 
   static GenericType fromSubclass(Class<?> subclass) {
-    return GenericTypeFromSubclass.SINGLETON.create(subclass);
+    return of(subclass.getGenericSuperclass());
   }
 
   <T> Class<T> getTypeArg(int index);
