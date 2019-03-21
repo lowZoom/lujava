f060c1fd66d883921dec76e0f41b5de01436f2d3
luj <david_lu_st@163.com>
2019-03-21 20:39:05 +0800

    添加类型构造方法

--- lujava/src/main/java/luj/ava/reflect/type/TypeX.java
+++ lujava/src/main/java/luj/ava/reflect/type/TypeX.java
@@ -8,6 +8,10 @@ public interface TypeX<T> {
     return new TypeXImpl(TypeToken.of(type));
   }
 
+  static <T> TypeX<T> ofInstance(T instance) {
+    return (TypeX<T>) of(instance.getClass());
+  }
+
   <T1> TypeX<T1> getSupertype(Class<T1> superclass);
 
   <T1> TypeX<T1> getTypeParam(int index);
