fe7c3ef6b1b45906f21866a42875703e509d853f
luj <david_lu_st@163.com>
2020-10-19 16:52:59 +0800

    增加集合实用接口

--- lujava/src/main/java/luj/ava/collection/CollectionX.java
+++ lujava/src/main/java/luj/ava/collection/CollectionX.java
@@ -1,9 +1,15 @@
 package luj.ava.collection;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import java.util.List;
 import java.util.Set;
 
 public interface CollectionX {
+
+  static <T> List<T> nonNull(List<T> list) {
+    return (list == null) ? ImmutableList.of() : list;
+  }
 
   static <T> Set<T> nonNull(Set<T> set) {
     return (set == null) ? ImmutableSet.of() : set;
